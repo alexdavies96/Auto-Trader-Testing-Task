36287ab61ddf616de7a63689c5ad7c98be3ae783
alexdavies96 <alex.davies96@gmx.com>
2017-10-13 10:32:48 +0100

    FirstCommit

---
+++ Auto-Trader-Testing-Task/src/main/java/Auto_Trader_Website_Testing/Auto_Trader_Testing_Task/App.java
@@ -0,0 +1,13 @@
+package Auto_Trader_Website_Testing.Auto_Trader_Testing_Task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
